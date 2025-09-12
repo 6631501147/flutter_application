6912b55a6ded4bb483cab211bcf649f431f72834
6631501147 <6631501147@lamduan.mfu.ac.th>
2025-09-12 10:51:03 +0700

    designsimple

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 //import 'week05/singlechild.dart';
 //import 'week05/multiple.dart';
-import 'week05/multipletwo.dart';
+//import 'week05/multipletwo.dart';
+import 'week05/designOne.dart';
 void main(){
   runApp(MaterialApp(
-    home: MultipleTwo(),
+    home: designOne(),
   ),
   );
 }
---
+++ lib/week05/designOne.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class designOne extends StatelessWidget {
+  const designOne({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children:[
+          Container(width: 100, color: Colors.red,),
+          Expanded(child: Container(width: 100, color: Colors.teal,
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(width:100,height: 100, color: Colors.yellow,),
+              Container(width:100,height: 100, color: Colors.green,),
+            ],
+          ),
+          ),),
+          Container(width: 100, color: Colors.blue,),
+        ],
+      ),
+    );
+  }
+}
