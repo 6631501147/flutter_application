a455ab1972b1b9e851fd90b32c24b15a3edd8864
6631501147 <6631501147@lamduan.mfu.ac.th>
2025-09-24 23:47:40 +0700

    time

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 //import 'week06/button_demo.dart';
 //import 'week06/imageer.dart';
 //import 'week06/baking.dart';
-import 'week06/login.dart';
+//import 'week06/login.dart';
+import 'week06/timer.dart';
 void main(){
   runApp(MaterialApp(
-  home: LoginDN(),
+  home: TimerDemo(),
   ),
   );
 }
